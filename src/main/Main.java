9b2b15b893eb880f124987cb87012f8bc32fcd96
wendleypf <wendley.franca@ccc.ufcg.edu.br>
2017-07-20 15:54:53 -0300

    Começando analisador sintatico

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,21 +2,39 @@ package main;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.nio.file.Paths;
+
+import compiler.generated.Parser;
 import compiler.generated.Scanner;
+import java_cup.Lexer;
+import java_cup.runtime.Symbol;
 import util.Log;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String filePath = "examples/code.txt";
+		String rootPath = Paths.get("").toAbsolutePath().toString();
+		String filePath = "/examples/";
+		String sourcecode = rootPath + filePath + "code.txt";
+		Log.log(sourcecode);
 		try {
-			Scanner scanner = new Scanner(new BufferedReader(new FileReader(filePath)));
+			Scanner scanner = new Scanner(new BufferedReader(new FileReader(sourcecode)));
+			
+			Parser parser = new Parser(scanner);
+			Symbol s = null;
+			try {
+				s = parser.parse();
+		    } catch (Exception e) {
+		        System.out.println(e.getMessage());
+		        return;
+		    }
+			System.out.println(s);
 		} catch (Exception e) {
 			Log.logErro(e.getMessage());
 			e.printStackTrace();
 		}
 			
-		Log.log("Compilação efetuada com sucesso.");
+		Log.log("CompilaÃ§Ã£o efetuada com sucesso.");
 	}
 
 }
